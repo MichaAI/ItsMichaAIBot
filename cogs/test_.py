55921d89a3ddc9ad2e7cb36c877e5ebf03157bb2
CommandLine <71168720+Def-Try@users.noreply.github.com>
2023-10-25 23:24:12 +0700

    Update test_.py

--- cogs/test_.py
+++ cogs/test_.py
@@ -1,7 +1,7 @@
 import discord
 from discord.ext import commands
 
-class test(commands.Cog):
+class test_(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
@@ -13,5 +13,9 @@ class test(commands.Cog):
     async def lol(self, ctx):
         await ctx.respond("Gjitk d gbple vhfpm") # я знаю что тут написано :moon_face:
 
+    @bot.slash_command(name='тест', description='Что-то делает.', guild_ids=None)
+    async def test_(ctx):
+        await ctx.respond('Успешный тест!')
+
 def setup(bot):
-    bot.add_cog(test(bot))
+    bot.add_cog(test_(bot))
