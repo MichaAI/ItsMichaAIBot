f4fac4564d8ebadc03693a8e796856c6fda7244e
MichaAI <121772662+MichaAI@users.noreply.github.com>
2023-10-25 21:57:44 +0300

    Это было лишнее

--- cogs/test_.py
+++ cogs/test_.py
@@ -5,14 +5,6 @@ class test_(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.slash_command()
-    async def test(self, ctx):
-        await ctx.respond("Успешный тест")
-
-    @commands.slash_command()
-    async def lol(self, ctx):
-        await ctx.respond("Gjitk d gbple vhfpm") # я знаю что тут написано :moon_face:
-
     @bot.slash_command(name='тест', description='Что-то делает.', guild_ids=None)
     async def test_(ctx):
         await ctx.respond('Успешный тест!')
