32b085439e3bba902791e3108a4e50d4718fd2b9
Deftry <nikolajkozin15@gmail.com>
2023-10-27 10:59:22 +0700

    hard sex with ArtiFurry

--- main.py
+++ main.py
@@ -1,17 +1,25 @@
 import discord
 from discord.ext import commands
-import coreData
 import os
+
 SETTINGS = {
-    'token': coreData.token,
+    'token': None,
 #    'bot': 'Its MichaAIs bot', # нигде не используются??
 #    'id': 1147250594776612948,
     'prefix': '*',
-    
+
     'log_messages': False,
     'owner_id': 629999906429337600,
     'persona_prefix': "Mb:"
 }
+
+try:
+    with open("./token.txt", 'r') as f:
+        SETTINGS['token'] = f.readlines()[0]
+except Exception as e:
+    print(f"ERROR! Failed to read tokenfile: {e}")
+    exit(1)
+
 bot = commands.Bot(command_prefix=commands.when_mentioned_or(SETTINGS['prefix']), intents=discord.Intents.all())
 
 @bot.event
@@ -25,7 +33,7 @@ async def on_message(message):
     if message.author == bot.user: return
 
     await bot.process_commands(message)
-    
+
     #if bot.user.mention in message.content: # зачем засорять консоль?
     #    print(message.author.mention)
     if message.content.startswith(SETTINGS['persona_prefix']) and message.author.id == SETTINGS['owner_id']:
@@ -39,7 +47,7 @@ async def reload_cogs(ctx):
         if not f.endswith(".py"): continue
         if "cogs." + f[:-3] in bot.extensions:
             try:
-                bot.unload_extensionf(f"cogs.{f[:-3]}")
+                bot.unload_extension(f"cogs.{f[:-3]}")
                 bot.load_extension(f"cogs.{f[:-3]}")
                 await ctx.send(f"cog {f[:-3]} successfully reloaded")
                 print(f"Cog {f[:-3]} reloaded")
@@ -52,14 +60,17 @@ async def reload_cogs(ctx):
                 await ctx.send(f"cog {f[:-3]} successfully loaded")
                 print(f"Cog {f[:-3]} loaded")
             except Exception as e:
-                await ctx.send(f"Failed to load cog {f[:-3]} - {e}"))
+                await ctx.send(f"Failed to load cog {f[:-3]} - {e}")
                 print(f"ERROR! Cog {f[:-3]} loading failed: {e}")
-        except Exception as e:
 
 def main():
     for f in os.listdir("./cogs"):
         if f.endswith(".py"):
-            bot.load_extension("cogs." + f[:-3])
+            try:
+                bot.load_extension("cogs." + f[:-3])
+                print(f"Cog {f[:-3]} loaded")
+            except Exception as e:
+                print(f"ERROR! Cog {f[:-3]} loading failed: {e}")
 
     bot.run(SETTINGS['token'])
 
