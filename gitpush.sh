9ec9e3e5b99ea9c0a70d18b34fb48986e0c564cb
CommandLine <71168720+Def-Try@users.noreply.github.com>
2023-10-31 17:18:13 +0700

    Delete gitpush.sh

--- gitpush.sh
+++
@@ -1,6 +0,0 @@
-mv token.txt ..
-echo "lol" > token.txt
-echo "" >> token.txt
-echo "DO NOT SHARE THIS FILE WITH ANYONE - THIS IS YOUR BOT TOKEN THAT CAN BE USED FOR BAD THINGS IF NOT IN THE RIGHT HANDS!" >> token.txt
-git push
-mv ../token.txt .
