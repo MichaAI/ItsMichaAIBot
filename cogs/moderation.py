a5994e041392066a98084fb7289b31691da48481
CommandLine <71168720+Def-Try@users.noreply.github.com>
2023-10-31 15:00:32 +0700

    Начало перехода на mongodb

--- cogs/moderation.py
+++ cogs/moderation.py
@@ -1,16 +1,12 @@
 import discord
 import asyncio
 from discord.ext import commands
-import sqlite3
-
-conn = sqlite3.connect('../database.db')  # Подключение к базе данных
-cursor = conn.cursor()
-cursor.execute("CREATE TABLE IF NOT EXISTS mute_role (id INTEGRER PRIMARY KEY, server_id INTEGRER, mute_role_id INTEGRER)")
-cursor.execute("CREATE TABLE IF NOT EXISTS mute (id INTEGER PRIMARY KEY, server_id INTEGER, mute_role_id INTEGER, member_id INTEGER, end_time INTEGER)")
+import pymongo
 
 class moderation(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+        self.db_client = self.bot.dbclient
         self.mute_tasks = {}
 
     @commands.slash_command()
