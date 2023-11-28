f6372fe3ea155c719893e5d7f35a8e7af2cdf70d
1 <misha-8-filatov@yandex.ru>
2023-11-28 16:05:42 +0300

    тесты ||(имитация работы)||

--- game_logic/game_dbwork.py
+++ game_logic/game_dbwork.py
@@ -2,16 +2,15 @@ import redis
 import pymongo
 
 r = redis.StrictRedis(
-    host="michaai.tplinkdns.com",
+    host="localhost",
     port="6379",
     db=0
 )
+
 
 def game_get_from_db(player_id: int):
     a = r.get(str(player_id) + ".game")
     return a
 
+
 print(game_get_from_db(4575647))
-
-
-
