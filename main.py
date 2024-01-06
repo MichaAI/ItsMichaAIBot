ed63092de5ce0cf9487cd29f6d91f2bc3940100a
CommandLine <71168720+Def-Try@users.noreply.github.com>
2024-01-07 02:50:36 +0700

    Update main.py

--- main.py
+++ main.py
@@ -43,7 +43,7 @@ async def on_ready():
 @bot.event
 async def on_message(message):
     if message.author.id == 891289716501119016
-        message.delete 
+        await message.delete() #если пытаешься бвть злым, пытайся лучше...
     if SETTINGS['log_messages']:
         print(f"Сервер: {message.guild.name} Канал: {message.channel.name} Автор:"
               f" {message.author.name} Сообщение: {message.content}")
