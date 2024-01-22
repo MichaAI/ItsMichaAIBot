b713e010e4e5deec10ecc28184b98e9f81135d17
MichaAI <misha-8-filatov@yandex.ru>
2024-01-22 16:13:27 +0300

    ну не можем убрать звезды, уберем все реакции.

--- cogs/star_remover.py
+++ cogs/star_remover.py
@@ -7,19 +7,17 @@ class StarRemove(commands.Cog):
         self.bot = bot
 
     @commands.Cog.listener()
-    async def on_raw_reaction_add(self, payload: discord.RawReactionActionEvent):
+    async def on_raw_reaction_add(self,
+                                     payload: discord.RawReactionActionEvent):
+    
         if payload.guild_id != 1076117733428711434:
             return
 
-        if payload.emoji != discord.PartialEmoji(name="⭐"):
-            return
-        message = await self.bot.get_channel(payload.channel_id).fetch_message(
-            payload.message_id
-        )
-        star_reactions = list(filter(lambda r: r.emoij == "⭐", message.reactions))
-        star_reaction = star_reactions[0]
-        await star_reaction.clear()
+        message = await self.bot.get_channel(payload.channel_id).fetch_message(payload.message_id)
 
+        for reaction in message.reactions:
+            print(reaction, message.reactions)
+            await reaction.clear()
 
 def setup(bot):
     bot.add_cog(StarRemove(bot))
