c097e90dcddb3cc4bc2f6714036ed1fd6750c4aa
MichaAI <MichaAI@users.noreply.github.com>
2024-01-28 19:24:36 +0000

    Apply automatic changes

--- cogs/role_tools.py
+++ cogs/role_tools.py
@@ -7,49 +7,60 @@ class RoleTools(commands.Cog):
         self.bot = bot
 
     @commands.slash_command()
-    async def give_role(self, ctx,
-                        member: discord.Member, role: discord.Role):
-        if not (ctx.author.guild_permissions.administrator or
-                ctx.author.guild_permissions.manage_roles):
+    async def give_role(self, ctx, member: discord.Member, role: discord.Role):
+        if not (
+            ctx.author.guild_permissions.administrator
+            or ctx.author.guild_permissions.manage_roles
+        ):
             await ctx.respond(content="У вас нет полномочий на выдачу ролей.")
             return
 
         if ctx.author.top_role <= role:
             await ctx.respond(
                 content=f"Вы не можете выдать роль {role} потому что она"
-                "находится выше вашей самой высокой роли.")
+                "находится выше вашей самой высокой роли."
+            )
             return
 
         if role in member.roles:
             await ctx.respond(
                 content=f"Вы не можете выдать роль {role} "
-                "потому что у участника {member.display_name} ({member}) уже есть эта роль.")
+                "потому что у участника {member.display_name} ({member}) уже есть эта роль."
+            )
             return
 
         await member.add_roles(role)
-        await ctx.respond(content=f"Пользователю {member.display_name} выдана роль {role}.")
+        await ctx.respond(
+            content=f"Пользователю {member.display_name} выдана роль {role}."
+        )
 
     @commands.slash_command()
     async def remove_role(self, ctx, member: discord.Member, role: discord.Role):
-        if not (ctx.author.guild_permissions.administrator or
-                ctx.author.guild_permissions.manage_roles):
+        if not (
+            ctx.author.guild_permissions.administrator
+            or ctx.author.guild_permissions.manage_roles
+        ):
             await ctx.respond(content="У вас нет полномочий на снятие ролей.")
             return
 
         if ctx.author.top_role <= role:
             await ctx.respond(
                 content=f"Вы не можете снять роль {role} потому "
-                "что она находится выше вашей самой высокой роли.")
+                "что она находится выше вашей самой высокой роли."
+            )
             return
 
         if role not in member.roles:
             await ctx.respond(
                 content=f"Вы не можете снять роль {role} потому что"
-                        "у участника {member.display_name} ({member}) отсутствует эта роль.")
+                "у участника {member.display_name} ({member}) отсутствует эта роль."
+            )
             return
 
         await member.remove_roles(role)
-        await ctx.respond(content=f"У пользователя {member.display_name} снята роль {role}.")
+        await ctx.respond(
+            content=f"У пользователя {member.display_name} снята роль {role}."
+        )
 
 
 def setup(bot):
