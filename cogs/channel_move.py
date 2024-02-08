2e50b6fbca8ae62e9038278b561aa08a5e3ce2a3
MichaAI <misha-8-filatov@yandex.ru>
2024-02-08 11:53:31 +0300

    тест склонений

--- cogs/channel_move.py
+++ cogs/channel_move.py
@@ -28,11 +28,12 @@ class ChannelMove(commands.Cog):
             or ctx.author.guild_permissions.administrator
         ):
             return await ctx.respond("Недостаточно полномочий.")
-
+        channel_pos = str(move_to)
+        channel_name = "канал" if channel_pos.endswith("1") else "каналов" if channel_pos[-2:] not in ["12", "13", "14"] else "канала"
         await channel.edit(position=channel.position - move_to)
         await ctx.respond(
             f"Позиция канала {channel.mention} "
-            f"успешно изменена на {abs(move_to)} каналов "
+            f"успешно изменена на {abs(move_to)} {channel_name} "
             f'{"вверх" if move_to>0 else "вниз"}'
         )
 
