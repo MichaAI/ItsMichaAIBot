88dd856dca5f2f0d24b402a000460242b5d62862
MichaAI <MichaAI@users.noreply.github.com>
2024-02-09 20:04:32 +0000

    Apply automatic changes

--- cogs/fun.py
+++ cogs/fun.py
@@ -10,13 +10,14 @@ class Fun(commands.Cog):
     async def hi(self, ctx):
         await ctx.reply(f"Здравствуй, {ctx.message.author.mention}!")
 
-
     @commands.command(aliases=["bаn", "бaн"])
     async def hi(self, ctx, *, reason: str = "None"):
         if not ctx.message.reference:
             return
         original = await ctx.fetch_message(ctx.message.reference.message_id)
-        await ctx.reply(f"Пользователь {original.author.mention} был забанен пользователем {ctx.message.author.mention} по причине `{reason}`")
+        await ctx.reply(
+            f"Пользователь {original.author.mention} был забанен пользователем {ctx.message.author.mention} по причине `{reason}`"
+        )
 
 
 def setup(bot):
