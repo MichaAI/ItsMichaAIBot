d7e2a9b9853c794b268888bc04fc5b65a2ffbf76
MichaAI <misha-8-filatov@yandex.ru>
2024-02-24 22:03:11 +0300

    БЛЯДЬ Я ЗАБЫЛ ПРО ТО ЧТО СООБЩЕНИЕ МОЖЕТ БЫТЬ В NSFW КАНАЛЕ

--- cogs/pseudoai.py
+++ cogs/pseudoai.py
@@ -4,30 +4,33 @@ from discord.ext import commands
 from redis import asyncio as aioredis
 from math import ceil, log2
 
-r = aioredis.StrictRedis(host="redis1", port=6380, decode_responses=True)
+r = aioredis.StrictRedis(host="localhost", port=6379, decode_responses=True)
 
-class Template(commands.Cog):
+class Pseudoai(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
     @commands.Cog.listener()
     async def on_message(self, message: discord.Message):
+        if message.channel.is_nsfw()
+            return
+
         msg_split = message.content.split()
         content = {i:msg_split.count(i) for i in msg_split}
 
         for key, value in content.items():
-            await r.set(key, 0, 600*value)
+            ttl = await r.ttl(key)
+            await r.set(key, 0, 600*value + ttl)
         
         if message.author == self.bot.user:
             return
 
-        if random.randint(1, 20) != 1:
+        if random.randint(1, 30) != 1:
             return
 
-        key_count = await r.dbsize()
         keys = []
 
-        for _ in range(ceil(log2(key_count)) + 2):
+        for _ in range(random.randint(5, 15)):
             key = await r.randomkey()
             keys.append(key)
             ttl = await r.ttl(key)
@@ -37,4 +40,4 @@ class Template(commands.Cog):
 
 
 def setup(bot):
-    bot.add_cog(Template(bot))
+    bot.add_cog(Pseudoai(bot))
