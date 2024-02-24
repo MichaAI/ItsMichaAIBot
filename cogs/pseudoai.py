88519092d61337acf7d26572c171a222ba937ca9
MichaAI <misha-8-filatov@yandex.ru>
2024-02-24 22:12:43 +0300

    а вы забывали двоеточие?

--- cogs/pseudoai.py
+++ cogs/pseudoai.py
@@ -13,7 +13,7 @@ class Pseudoai(commands.Cog):
 
     @commands.Cog.listener()
     async def on_message(self, message: discord.Message):
-        if message.channel.is_nsfw()
+        if message.channel.is_nsfw():
             return
 
         msg_split = message.content.split()
