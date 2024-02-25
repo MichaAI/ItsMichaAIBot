4f2b7b562939e9c469523d2b50600d4053b027f6
MichaAI <MichaAI@users.noreply.github.com>
2024-02-25 07:23:36 +0000

    Apply automatic changes

--- cogs/fun.py
+++ cogs/fun.py
@@ -102,7 +102,11 @@ class Fun(commands.Cog):
     @commands.Cog.listener()
     async def on_message(self, message):
 
-        if message.channel.id not in (1076117734208835606, 1106541600034783332, 1073641946129641584):
+        if message.channel.id not in (
+            1076117734208835606,
+            1106541600034783332,
+            1073641946129641584,
+        ):
             return
 
         if random.randint(1, 200) != 200:
