2964b7591b506ff708b637d1858b040b39497dc5
MichaAI <MichaAI@users.noreply.github.com>
2024-02-28 16:56:15 +0000

    Apply automatic changes

--- cogs/fastsdxl.py
+++ cogs/fastsdxl.py
@@ -3,6 +3,7 @@ import aiohttp
 import discord
 from discord.ext import commands
 
+
 class Fastsdxl(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
@@ -10,11 +11,14 @@ class Fastsdxl(commands.Cog):
     @commands.command(aliases=["image"])
     async def fastsdxl_command(self, ctx: commands.Context, *, promt: str):
         async with aiohttp.ClientSession() as session:
-            response = await session.post(url="https://fal.run/fal-ai/fast-lightning-sdxl",
-                                          headers={"Content-Type":"application/json",
-                                                   "Authorization": f"Key {self.bot.fal_key}"},
-                                          data=json.dumps({'prompt': promt}), 
-                                          )
+            response = await session.post(
+                url="https://fal.run/fal-ai/fast-lightning-sdxl",
+                headers={
+                    "Content-Type": "application/json",
+                    "Authorization": f"Key {self.bot.fal_key}",
+                },
+                data=json.dumps({"prompt": promt}),
+            )
         response = await response.json()
         if ctx.channel.nsfw:
             await ctx.reply(response["images"][0]["url"])
@@ -24,28 +28,53 @@ class Fastsdxl(commands.Cog):
             return
         await ctx.reply("Ай как плохо такое генерировать")
 
-
     @commands.slash_command(name="image", description="fast image generator")
-    async def move_channel(self, ctx, promt: str, number_images: int = 1, negative: str = ""):
+    async def move_channel(
+        self, ctx, promt: str, number_images: int = 1, negative: str = ""
+    ):
         if number_images > 4:
-            await ctx.respond("Слишком много изображений, макксимум `4` изображения в одном запросе", ephimeral=True)
+            await ctx.respond(
+                "Слишком много изображений, макксимум `4` изображения в одном запросе",
+                ephimeral=True,
+            )
             return
 
         async with aiohttp.ClientSession() as session:
-            response = await session.post(url="https://fal.run/fal-ai/fast-lightning-sdxl",
-                                          headers={"Content-Type":"application/json",
-                                                   "Authorization": f"Key {self.bot.fal_key}"},
-                                          data=json.dumps({'prompt': promt,
-                                                           'num_images': number_images,
-                                                           'negative_prompt': negative}), 
-                                          )
+            response = await session.post(
+                url="https://fal.run/fal-ai/fast-lightning-sdxl",
+                headers={
+                    "Content-Type": "application/json",
+                    "Authorization": f"Key {self.bot.fal_key}",
+                },
+                data=json.dumps(
+                    {
+                        "prompt": promt,
+                        "num_images": number_images,
+                        "negative_prompt": negative,
+                    }
+                ),
+            )
         response = await response.json()
         if ctx.channel.nsfw:
-            await ctx.respond(" ".join([response["images"][x]["url"] for x in range(len(response["images"]))]))
+            await ctx.respond(
+                " ".join(
+                    [
+                        response["images"][x]["url"]
+                        for x in range(len(response["images"]))
+                    ]
+                )
+            )
             return
         if not any(response["has_nsfw_concepts"]):
-            await ctx.respond(" ".join([response["images"][x]["url"] for x in range(len(response["images"]))]))
-        await ctx.respond("Ой как плохо такое генерировать, осуждаю.", ephimeral = True)
+            await ctx.respond(
+                " ".join(
+                    [
+                        response["images"][x]["url"]
+                        for x in range(len(response["images"]))
+                    ]
+                )
+            )
+        await ctx.respond("Ой как плохо такое генерировать, осуждаю.", ephimeral=True)
 
 
 def setup(bot):
